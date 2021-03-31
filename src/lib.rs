ad1fec5e3b6b775944756c763cf3b73cca7f7d4a
Rishvic Pushpakaran <rishvic@gmail.com>
2021-03-31 21:21:38 +0530

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, maze-generator-two!");
+}
